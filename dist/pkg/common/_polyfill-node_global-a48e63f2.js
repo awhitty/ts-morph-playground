f55f8edb2ced2f91ad76f770cbb4132c787b0464
Austin Whittier <austin.whitt@gmail.com>
2021-04-11 23:36:06 -0700

    Updates

--- dist/pkg/common/_polyfill-node_global-a48e63f2.js
+++
@@ -1,6 +0,0 @@
-var global = (typeof global !== "undefined" ? global :
-  typeof self !== "undefined" ? self :
-  typeof window !== "undefined" ? window : {});
-
-export { global as g };
-//# sourceMappingURL=_polyfill-node_global-a48e63f2.js.map
--- dist/pkg/monaco-editor.js
+++
@@ -1,4 +0,0 @@
-export { l as languages } from './common/editor.main-1bbb8e5c.js';
-import './common/_polyfill-node_global-a48e63f2.js';
-import './common/_commonjsHelpers-8c19dec8.js';
-//# sourceMappingURL=monaco-editor.js.map
